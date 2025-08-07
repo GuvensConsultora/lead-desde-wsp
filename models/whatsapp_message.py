35e43637878e57b56d3671c819088029821448c7
GuvensConsultora <guvensconsultora@gmail>
2025-08-07 12:48:07 -0300

    Prueba error

--- models/whatsapp_message.py
+++ models/whatsapp_message.py
@@ -9,36 +9,5 @@ class WhatsAppMessage(models.Model):
     @api.model
     def create(self, vals):
         message = super().create(vals)
-        raise UserError("Este número está bloqueado y no se puede crear un lead.")
-
-
-
-        if message.direction == 'inbound' and message.phone:
-            partner = self.env['res.partner'].search([
-                ('mobile', '=', message.phone)
-            ], limit=1)
-
-            if not partner:
-                partner = self.env['res.partner'].create({
-                    'name': f"WhatsApp {message.phone}",
-                    'mobile': message.phone,
-                })
-
-            active_lead = self.env['crm.lead'].search([
-                ('partner_id', '=', partner.id),
-                ('active', '=', True),
-                ('stage_id.is_won', '=', False),
-                ('stage_id.is_lost', '=', False)
-            ], limit=1)
-
-            if not active_lead:
-                self.env['crm.lead'].create({
-                    'name': f"Nuevo lead desde WhatsApp ({message.phone})",
-                    'partner_id': partner.id,
-                    'contact_name': partner.name,
-                    'mobile': partner.mobile,
-                    'type': 'opportunity',
-                    'description': message.body or '',
-                })
 
         return message
