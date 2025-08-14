d790d5c8ef64767991a2a2e995a07fe0bf69ff82
GuvensConsultora <guvensconsultora@gmail>
2025-08-14 12:33:41 -0300

    Agregar sudo al create lead

--- models/whatsapp_message.py
+++ models/whatsapp_message.py
@@ -16,7 +16,7 @@ class WhatsAppMessage(models.Model):
 
 
         # ==== CREAR LEADS ====
-        lead = self.env['crm.lead'].create({
+        lead = self.env['crm.lead'].sudo().create({
             'name': rs_msj.subject or "Lead desde WHATSAPP",
             'phone': rs_msj.mobile_number,
             'description': rs_msj.body or '',
